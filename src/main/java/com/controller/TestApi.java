4fc7a79c8ea99761cf880e0c8b9a2af22f11437d
trunghq <trung.hoangquoc203@gmail.com>
2023-09-19 22:51:59 +0700

    update

--- src/main/java/com/controller/TestApi.java
+++ src/main/java/com/controller/TestApi.java
@@ -1,7 +1,13 @@
 package com.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestApi {
+
+    @GetMapping("/hello")
+    public String getHello() {
+        return "Hello trunghq";
+    }
 }
